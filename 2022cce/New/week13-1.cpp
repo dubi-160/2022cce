87b679f069cdeeba2f5b39ec6eb20c3ad470aa82
dubi-160 <112918682+dubi-160@users.noreply.github.com>
2023-05-09 11:49:13 +0800

    Add files via upload

--- 2022cce/New/week13-1.cpp
+++ 2022cce/New/week13-1.cpp
@@ -1,13 +1,33 @@
-#include <stdio.h>
-int a[50];
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+       vector<int> ans;
+       int M = matrix.size();
+       int N = matrix[0].size();
+       int i=0,j=0;
+       int dir=0;
 
-int main()
-{
-    a[0] = 0;
-    a[1] = 1;
-    printf("1 ");
-    for(int i=2; i<20; i++){
-        a[i] = a[i-1] + a[i-2];
-        printf("%d ", a[i]);
+        int di[4] = {0, 1, 0, -1};
+        int dj[4] = {1, 0, -1, 0};
+        int left=0, right=N-1, up=1,down=M-1;
+        for(int k=0;k<M*N; k++){
+            ans.push_back(matrix[i][j]);
+            if(dir==0 && j==right){
+                dir++;
+                right--;
+            }else if(dir==1 && i==down){
+                dir++;
+                down--;
+            }else if(dir==2 && j==left){
+                dir++;
+                left++;
+            }else if(dir==3 && i==up){
+                dir = 0;
+                up++;
+            }
+            i += di[dir];
+            j += dj[dir];
+        }
+        return ans;
     }
-}
+};
